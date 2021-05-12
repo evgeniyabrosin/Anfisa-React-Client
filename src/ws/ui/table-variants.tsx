e86e4d4839e8d5e6d8d2fe67404aa4e2246c6a17
Artem <artem.lomaev@bravedevelopers.com>
2021-05-12 13:07:12 +0400

    fix warnings eslint

--- src/ws/ui/table-variants.tsx
+++ src/ws/ui/table-variants.tsx
@@ -1,22 +1,13 @@
 /* eslint-disable react/jsx-key */
 import { observer } from 'mobx-react-lite'
-import { ReactElement, useMemo } from 'react'
+import { ReactElement } from 'react'
 import styled from 'styled-components'
 
-import { ANYType } from '../../..'
 import datasetStore from '../../store/dataset'
 import variantStore from '../../store/variant'
 import { Loader } from '../../ui/loader'
 import { variantColumnTable } from '../columns'
-import { FilterCell } from './filter-cell'
-import { GnomadCell } from './gnomad-cell'
-import { HG19Cell } from './hg19-cell'
-import { PredicationI, PredictionsCell } from './predictions-cell'
-import { ProteinChangeCell } from './protein-change-cell'
-import { QualityCell } from './quality-cell'
 import { Table } from './table'
-import { TagsCell } from './tags-cell'
-import { VariantCell } from './variant-cell'
 
 const Styles = styled.div`
   table {
