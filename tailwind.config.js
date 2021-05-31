9a3e3ecafcd3d889ad25de508b1354a1b19f3ea6
Roman Basharin <roman.basharin@quantori.com>
2021-05-31 14:28:22 +0300

    feat import theme/colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
+const themeColors = require('./src/theme/colors.js')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      ...themeColors,
+      blue: { ...themeColors.blue },
+      grey: { ...themeColors.grey },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-  corePlugins: ['margin', 'padding'],
 }
