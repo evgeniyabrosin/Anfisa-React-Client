0bdd96c527ee1215b4ee8d096cb0f9eb6fc0232e
Roman Basharin <roman.basharin@quantori.com>
2021-06-01 14:35:13 +0300

    feat: set tailwind screens + html 'min-width: 1024px'

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,14 @@ module.exports = {
     fontFamily: {
       sans: ['"Roboto"', 'sans-serif'],
     },
+    screens: {
+      xl2: { max: '1535px' }, // => @media (max-width: 1535px) { ... }
+      xl: { max: '1279px' }, // => @media (max-width: 1279px) { ... }
+      lg: { max: '1023px' }, // => @media (max-width: 1023px) { ... }
+      // Excluded
+      // 'md': { max: '767px' }, // => @media (max-width: 767px) { ... }
+      // 'sm': { max: '639px' }, // => @media (max-width: 639px) { ... }
+    },
   },
   variants: {
     appearance: [],
