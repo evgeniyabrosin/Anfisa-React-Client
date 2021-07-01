f59261c72b71f99564c530b4941e060b5aea9e9b
Artem <artem.lomaev@bravedevelopers.com>
2021-07-01 18:35:35 +0400

    fix outside click export button

--- src/ui/export-panel.tsx
+++ src/ui/export-panel.tsx
@@ -1,6 +1,7 @@
-import { ReactElement } from 'react'
+import { ReactElement, useRef } from 'react'
 
 import { ExportTypeEnum } from '@core/enum/export-type.enum'
+import { useOutsideClick } from '@core/hooks/use-outside-click'
 import { t } from '@i18n'
 import operationsStore from '@store/operations'
 
@@ -9,13 +10,20 @@ interface Props {
 }
 
 export const ExportPanel = ({ close }: Props): ReactElement => {
+  const ref = useRef<any>(null)
+
   const handleDownload = (type: ExportTypeEnum) => {
     operationsStore.exportReportAsync(type)
     close()
   }
 
+  useOutsideClick(ref, close)
+
   return (
-    <div className="bg-white text-black rounded mt-2 shadow-card text-12 cursor-pointer flex">
+    <div
+      className="bg-white text-black rounded mt-2 shadow-card text-12 cursor-pointer flex"
+      ref={ref}
+    >
       <span
         className="py-1 px-3 rounded hover:bg-blue-light"
         onClick={() => handleDownload(ExportTypeEnum.Excel)}
