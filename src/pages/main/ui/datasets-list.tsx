13e2511ef0c8fd6f163e6bae1fba21a40b2839bd
Artem <artem.lomaev@quantori.com>
2021-07-16 18:13:31 +0400

    add scroll to datasets

--- src/pages/main/ui/datasets-list.tsx
+++ src/pages/main/ui/datasets-list.tsx
@@ -1,14 +1,25 @@
-import { ReactElement } from 'react'
+import { ReactElement, useRef } from 'react'
 import { observer } from 'mobx-react-lite'
 
 import { DsDistItem } from '@declarations'
+import useWindowDimensions from '@core/hooks/use-window-dimensions'
 import dirinfoStore from '@store/dirinfo'
 import { DatasetsListItem } from './datasets-list-item'
 
 export const DatasetsList = observer(
   (): ReactElement => {
+    const { height } = useWindowDimensions()
+
+    const newRef = useRef<any>(null)
+
+    const offsetTop = newRef?.current?.offsetTop
+
     return (
-      <div>
+      <div
+        ref={newRef}
+        style={{ height: height - offsetTop }}
+        className="overflow-y-scroll overflow-x-hidden"
+      >
         {dirinfoStore.dsDistKeys.map(key => {
           const item: DsDistItem = dirinfoStore.dirinfo['ds-dict'][key]
 
