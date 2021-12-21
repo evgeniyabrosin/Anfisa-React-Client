423f0dd654904e478ccb47400c29f65c5781a06c
Andrey <andrey.ustyumenko@quantori.com>
2021-12-21 20:06:28 +0300

    fix: added select all handler

--- src/pages/filter/ui/query-builder/ui/modal-select-filters.tsx
+++ src/pages/filter/ui/query-builder/ui/modal-select-filters.tsx
@@ -55,6 +55,21 @@ export const ModalSelectFilters = observer(
 
     const [searchValue, setSearchValue] = useState('')
     const [currentPage, setCurrentPage] = useState(0)
+    const [isAllFiltersChecked, setIsAllFiltersChecked] = useState(false)
+
+    const handleCheckAll = (checked: boolean) => {
+      if (checked && isAllFiltersChecked) return
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      chunks[currentPage]?.forEach(([variantName]: [string, number]) => {
+        if (checked) {
+          dtreeStore.addSelectedFilter(variantName)
+        } else {
+          dtreeStore.removeSelectedFilter(variantName)
+        }
+
+        setIsAllFiltersChecked(checked)
+      })
+    }
 
     const handleChange = (value: string) => {
       setSearchValue(value)
@@ -103,7 +118,12 @@ export const ModalSelectFilters = observer(
 
             <ModsDivider />
 
-            <div className="text-blue-bright">{t('general.selectAll')}</div>
+            <div
+              className="cursor-pointer text-blue-bright"
+              onClick={() => handleCheckAll(true)}
+            >
+              {t('general.selectAll')}
+            </div>
 
             <ModsDivider />
 
