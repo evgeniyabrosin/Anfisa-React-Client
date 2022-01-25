1dc406a9a11ca798c0c1b2d059cc54675d32f7e0
Artem Khutoryan <artem.khutoryan@quantori.com>
2022-01-25 16:06:41 +0300

    test(FOROME-506): changes in main table test
    test(FOROME-506): updates for main table

--- cypress/page-objects/lib/timeouts.cy.tsx
+++ cypress/page-objects/lib/timeouts.cy.tsx
@@ -2,4 +2,6 @@ export enum Timeouts {
   TenSecondsTimeout = 10000,
   FifteenSecondsTimeout = 15000,
   TwentySecondsTimeout = 20000,
+  ThirtyFiveSecondsTimeout = 35000,
+  OneMinuteTimeout = 60000,
 }
