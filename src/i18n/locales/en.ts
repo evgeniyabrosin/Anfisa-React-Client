b559317837edeb80b4d8814682ab4539b5adc0bc
QSdmitrioul <dmitriy.skroba@quantori.com>
2022-03-28 12:40:24 +0300

    feat(FOROME-711): add error message on wrong value in compound request

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -196,6 +196,8 @@ export const en = {
     state: 'State',
     locus: 'Locus',
     chromosomeNameIsNotCorrect: 'Chromosome name is incorrect',
+    minimalCountsOfEventsOnCompoundRequest:
+      'Minimal count of events should be more than 0',
     fullList: 'Full list',
     samples25: 'Samples-25',
     editCurrentDecisionTreeCode: 'Edit current Decision Tree code',
