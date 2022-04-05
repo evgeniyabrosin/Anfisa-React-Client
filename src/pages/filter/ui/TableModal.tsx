a59297e788e6a12cb40f769cf83beaae58dba933
QSdmitrioul <dmitriy.skroba@quantori.com>
2022-04-05 17:33:18 +0300

    feat(FOROME-803): redesign view variants

--- src/pages/filter/ui/TableModal.tsx
+++ src/pages/filter/ui/TableModal.tsx
@@ -1,17 +1,20 @@
-import { Key, useEffect, useRef, useState } from 'react'
+import React, { Key, useEffect, useRef, useState } from 'react'
 import cn from 'classnames'
+import { get } from 'lodash'
 import { toJS } from 'mobx'
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
 
+import { IGridLayout } from '@declarations'
+import { t } from '@i18n'
 import datasetStore from '@store/dataset'
 import dtreeStore from '@store/dtree'
 import filterStore from '@store/filter'
 import variantStore from '@store/variant'
 import { Icon } from '@ui/icon'
+import { RadioButton } from '@ui/radio-button'
 import { VariantBody } from '@components/variant/ui/body'
 import { GlbPagesNames } from '@glb/glb-names'
-import { ControlPanelDivider } from '@pages/ws/ui/control-panel-divider'
 import { TCondition } from '@service-providers/common/common.interface'
 import { fetchDsListAsync } from '@utils/TableModal/fetchDsListAsync'
 import { fetchJobStatusAsync } from '@utils/TableModal/fetchJobStatusAsync'
@@ -31,6 +34,9 @@ const ModalContent = styled.div`
   width: 90%;
   min-height: 700px;
   max-height: 500px;
+  .react-grid-item {
+    width: calc(100% - 30px) !important;
+  }
 `
 
 type VariantsSize = 'SMALL' | 'MIDDLE' | 'LARGE'
@@ -41,22 +47,7 @@ export const TableModal = observer(() => {
   const [variantIndex, setVariantIndex] = useState(0)
   const [isSampleMode, setIsSampleMode] = useState(false)
   const [variantSize, setVariantSize] = useState<VariantsSize>()
-  const [drawerWidth, setDrawerWidth] = useState(0)
-  const leftPanelRef = useRef<HTMLDivElement>(null)
   const ref = useRef(null)
-
-  useEffect(() => {
-    function handleResize() {
-      setDrawerWidth(
-        window.innerWidth - (leftPanelRef.current?.clientWidth || 0) - 140,
-      )
-    }
-
-    console.log(leftPanelRef.current?.clientWidth)
-    window.addEventListener('resize', handleResize)
-
-    return () => window.removeEventListener('resize', handleResize)
-  }, [leftPanelRef.current?.clientWidth])
 
   useEffect(() => {
     variantStore.fetchVarinatInfoForModalAsync(datasetStore.datasetName, 0)
@@ -148,24 +139,81 @@ export const TableModal = observer(() => {
     setVariantList(newVariantList)
   }
 
+  const onCollapse = () => {
+    if (isCollapsed) {
+      const parents = document.querySelectorAll('#parent')
+
+      setLayout((prev: any[]) => {
+        const newLayout = prev.map((item: any, index: number) => ({
+          ...item,
+          h:
+            get(parents[index].children[1].firstChild, 'clientHeight', 0) *
+              0.0208 +
+            1.3,
+          y: index,
+        }))
+
+        window.sessionStorage.setItem('gridLayout', JSON.stringify(newLayout))
+
+        return newLayout
+      })
+      return
+    }
+
+    setLayout((prev: any[]) => {
+      const newLayout = prev.map((item: any) => ({
+        ...item,
+        h: 1,
+      }))
+
+      window.sessionStorage.setItem('gridLayout', JSON.stringify(newLayout))
+
+      return newLayout
+    })
+  }
+
+  const isCollapsed = JSON.parse(
+    window.sessionStorage.getItem('gridLayout') || '[]',
+  ).every((el: IGridLayout) => el.h === 1)
+
   return (
-    <ModalView className="bg-grey-blue" onClick={closeModal}>
+    <ModalView className="bg-grey-blue rounded-lg" onClick={closeModal}>
       <ModalContent
-        className="flex flex-col bg-white rounded-lg overflow-y-auto"
+        className="flex flex-col bg-white rounded-lg overflow-y-auto h-full"
         ref={ref}
       >
         {isLoading ? (
-          jobStatus[1]
+          <div className="flex w-full h-full rounded-lg items-center justify-around">
+            {jobStatus[1]}
+          </div>
         ) : (
           <>
-            <div className="flex w-full overflow-hidden">
-              <div
-                className="flex flex-col rounded-l-lg overflow-auto"
-                ref={leftPanelRef}
-              >
+            <div className="flex w-full overflow-hidden rounded-lg">
+              <div className="flex flex-col rounded-l-lg overflow-auto">
+                <div className="flex px-[14px] py-4">
+                  <div className="flex items-center mr-[14px]">
+                    <RadioButton
+                      isDisabled={variantSize === 'LARGE'}
+                      isChecked={!isSampleMode}
+                      onChange={toggleMode}
+                    />
+
+                    <div className="ml-[6px]">{t('dtree.fullList')}</div>
+                  </div>
+
+                  <div className="flex items-center">
+                    <RadioButton
+                      isDisabled={variantSize === 'SMALL'}
+                      isChecked={isSampleMode}
+                      onChange={toggleMode}
+                    />
+
+                    <p className="ml-[6px]">{t('dtree.samples25')}</p>
+                  </div>
+                </div>
                 <table className="min-w-full table-auto">
                   <thead>
-                    <tr className="border-b-[1px] border-t-[1px] border-blue-dark">
+                    <tr className="border-y-[0.5px] border-grey-blue">
                       <th className="px-4 py-3">Gene</th>
                       <th className="px-4 py-3">Variant</th>
                     </tr>
@@ -177,7 +225,7 @@ export const TableModal = observer(() => {
                         index: number,
                       ) => {
                         const parts = variant.lb
-                          .split(/\[|\]/)
+                          .split(/[[\]]/)
                           .filter(Boolean)
                           .map(el => el.trim())
                         const gene = parts[0]
@@ -186,7 +234,9 @@ export const TableModal = observer(() => {
                         return (
                           <tr
                             key={variant.no}
-                            className={cn('', { 'bg-blue-bright': active })}
+                            className={cn('border-y-[0.5px] border-grey-blue', {
+                              'bg-blue-bright': active,
+                            })}
                             onClick={() => setVariantIndex(index)}
                           >
                             <th
@@ -207,26 +257,30 @@ export const TableModal = observer(() => {
               <div className="flex flex-col rounded-r-lg w-full">
                 <div className="flex px-4 py-2 justify-between w-full bg-blue-dark">
                   <div
-                    className="flex justify-center text-16 font-semibold text-blue-bright"
+                    className="flex justify-center text-16 font-semi-bold text-blue-bright"
                     dangerouslySetInnerHTML={{
                       __html: variantList[variantIndex]?.lb ?? '',
                     }}
                   />
                   <div className="flex justify-center items-center">
+                    <Icon
+                      name={isCollapsed ? 'Expand' : 'Collapse'}
+                      onClick={onCollapse}
+                      size={16}
+                      className="cursor-pointer text-white"
+                    />
                     <div className="bg-blue-lighter mx-3 rounded-sm w-0.5 h-[20px]" />
                     <Icon
                       name="Close"
-                      onClick={
-                        toggleMode /*() => dtreeStore.closeTableModal()*/
-                      }
+                      onClick={() => dtreeStore.closeTableModal()}
                       size={16}
-                      className="cursor-pointer"
+                      className="cursor-pointer text-white"
                     />
                   </div>
                 </div>
                 <div className="flex flex-col bg-blue-lighter w-full h-full overflow-auto">
                   <VariantBody
-                    drawerWidth={drawerWidth}
+                    drawerWidth={window.innerWidth}
                     setLayout={setLayout}
                     layout={layout}
                   />
