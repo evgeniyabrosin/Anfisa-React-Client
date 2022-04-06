a58f088f29fdb64d732b027d00f16327e0a544e2
QSdmitrioul <dmitriy.skroba@quantori.com>
2022-04-06 18:28:16 +0300

    fix(FOROME-761): extract empty value as constant

--- src/pages/ws/columns.ts
+++ src/pages/ws/columns.ts
@@ -62,3 +62,5 @@ export const variantColumnTable = [
     Cell: CellFilter,
   },
 ]
+
+export const EMPTY_VALUE = '-'
--- src/pages/ws/ui/cell-population.tsx
+++ src/pages/ws/ui/cell-population.tsx
@@ -1,13 +1,14 @@
 import { ReactElement } from 'react'
 import get from 'lodash/get'
 
+import { EMPTY_VALUE } from '@pages/ws/columns'
 import { CellI, PredicationI } from './cell-interfaces'
 import { GnomadItem } from './gnomad-item'
 
 export const CellPopulation = ({ cell }: CellI): ReactElement => {
   const gnomads = get(cell, 'value', []) as PredicationI[]
 
-  if (!gnomads.length) return <div>{'-'}</div>
+  if (!gnomads.length) return <div>{EMPTY_VALUE}</div>
 
   return (
     <div>
