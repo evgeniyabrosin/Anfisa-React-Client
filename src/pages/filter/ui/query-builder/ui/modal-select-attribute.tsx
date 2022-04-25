e7a8df07ced53f70939a749fd6084e67777fe6bd
Alexander Malyshev <98574721+AlMaQntr@users.noreply.github.com>
2022-04-25 10:47:01 +0500

    feat(FOROME-827): implemented prediction power indicator (#578)
    * refactor(FOROME-850): implemented base store for async data
    
    * refactor(FOROME-850): refactored filter refiner store
    
    * refactor(FOROME-850): fixed dtree preset menu
    
    * fix(FOROME-850): fixed pr comment
    
    * feat(FOROME-827): added prediction power indicator
    
    * feat(FOROME-827): added prediction power indicator for groups
    
    * fix(FOROME-827): merged develop branch
    
    * feat(FOROME-827): removed unused file

--- src/pages/filter/ui/query-builder/ui/modal-select-attribute.tsx
+++ src/pages/filter/ui/query-builder/ui/modal-select-attribute.tsx
@@ -20,10 +20,6 @@ export const ModalSelectAttribute = observer((): ReactElement => {
     storageId: 'attributesModalScrollPos',
   })
 
-  const groupNames = Object.keys(filteredQueryBuilder)
-
-  const subGroupData = Object.values(filteredQueryBuilder)
-
   const modalBaseRef = useRef(null)
 
   const handleClose = () => {
@@ -57,11 +53,12 @@ export const ModalSelectAttribute = observer((): ReactElement => {
             {t('dtree.loading')}
           </div>
         ) : (
-          groupNames.map((groupName, index) => (
+          filteredQueryBuilder.map(({ name, attributes, power }) => (
             <QueryBuilderSubgroup
-              groupName={groupName}
-              subGroupData={subGroupData[index]}
-              key={groupName}
+              key={name}
+              groupName={name}
+              predictionPower={power}
+              subGroupData={attributes}
               changeIndicator={dtreeStore.filterModalChangeIndicator}
               isContentExpanded={dtreeStore.isFilterModalContentExpanded}
               isModal
