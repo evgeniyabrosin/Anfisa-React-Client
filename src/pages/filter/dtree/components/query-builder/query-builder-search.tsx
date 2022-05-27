367f340570e2a2d38f24d9ece9c5e76c79fdf4ec
AndreyUstyumenko <96476703+AndreyUstyumenko@users.noreply.github.com>
2022-05-27 18:20:23 +0300

    feat(FOROME-1064): Added divider after switcher (#653)

--- src/pages/filter/dtree/components/query-builder/query-builder-search.tsx
+++ src/pages/filter/dtree/components/query-builder/query-builder-search.tsx
@@ -4,6 +4,7 @@ import { observer } from 'mobx-react-lite'
 
 import { t } from '@i18n'
 import dtreeStore from '@store/dtree'
+import { Divider } from '@ui/divider'
 import { Icon } from '@ui/icon'
 import { Switch } from '@ui/switch'
 import { DecisionTreesResultsDataCy } from '@components/data-testid/decision-tree-results.cy'
@@ -64,15 +65,18 @@ export const QueryBuilderSearch = observer(
         {!isSubgroupItemSearch && (
           <div className="flex items-center justify-between pl-2 text-grey-blue">
             {showSwitcher && (
-              <div className="mr-2 flex items-center">
-                <Switch
-                  isChecked={!!isSwitched}
-                  onChange={(e: boolean) => onSwitch && onSwitch(e)}
-                  className="mr-2"
-                />
+              <>
+                <div className="mr-2 flex items-center">
+                  <Switch
+                    isChecked={!!isSwitched}
+                    onChange={(e: boolean) => onSwitch && onSwitch(e)}
+                    className="mr-2"
+                  />
 
-                <span className="text-sm">{t('filter.switcher')}</span>
-              </div>
+                  <span className="text-sm">{t('filter.switcher')}</span>
+                </div>
+                <Divider orientation="vertical" color="light" spacing="dense" />
+              </>
             )}
 
             <div className="mr-2">
