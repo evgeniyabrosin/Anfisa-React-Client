156c628ac08d63629784b2a7a600c88c5567c809
QSdmitrioul <dmitriy.skroba@quantori.com>
2022-05-27 18:31:11 +0300

    feat(FOROME-97): move useEffect to hook

--- src/ui/shadow-scroller/shadow-scroller.tsx
+++ src/ui/shadow-scroller/shadow-scroller.tsx
@@ -1,16 +1,10 @@
 import styles from './shadow-scroller.module.css'
 
-import { FC, useEffect, useRef } from 'react'
+import { FC, useRef } from 'react'
 import cn, { Argument } from 'classnames'
 
 import { ScrollDirection, useGrabScroll } from '@core/hooks/use-grab-scroll'
-import {
-  createShadow,
-  createTrigger,
-  DisplayValue,
-  hide,
-  Placement,
-} from '@ui/shadow-scroller/shadow-scroller.utils'
+import { useScrollShadows } from '@ui/shadow-scroller/shadow-scroller.hook'
 
 interface IScrollerProp {
   direction?: ScrollDirection
@@ -30,96 +24,7 @@ export const ShadowScroller: FC<IScrollerProp> = ({
 
   const onMouseDown = useGrabScroll(ref, direction)
 
-  useEffect(() => {
-    const scrollable = ref.current
-    const shadows = shadowsRef.current
-
-    if (!scrollable || !shadows) {
-      return
-    }
-
-    const area = scrollable?.firstElementChild as HTMLDivElement
-
-    if (!area) {
-      return
-    }
-
-    const topTrigger = createTrigger(area, Placement.top)
-    const rightTrigger = createTrigger(area, Placement.right)
-    const bottomTrigger = createTrigger(area, Placement.bottom)
-    const leftTrigger = createTrigger(area, Placement.left)
-
-    const topShadow = createShadow(shadows, Placement.top, direction)
-    const rightShadow = createShadow(shadows, Placement.right, direction)
-    const bottomShadow = createShadow(shadows, Placement.bottom, direction)
-    const leftShadow = createShadow(shadows, Placement.left, direction)
-
-    const resizeObserver = new ResizeObserver(entries => {
-      const {
-        contentRect: { width, height },
-      } = entries[0]
-      shadows.style.width = `${width}px`
-      shadows.style.height = `${height}px`
-    })
-
-    resizeObserver.observe(scrollable)
-
-    const intersectionObserver = new IntersectionObserver(
-      entries => {
-        for (const entry of entries) {
-          switch (entry.target) {
-            case topTrigger:
-              topShadow.style.display =
-                entry.isIntersecting || hide(Placement.top, direction)
-                  ? DisplayValue.none
-                  : DisplayValue.block
-              break
-            case rightTrigger:
-              rightShadow.style.display =
-                entry.isIntersecting || hide(Placement.right, direction)
-                  ? DisplayValue.none
-                  : DisplayValue.block
-              break
-            case bottomTrigger:
-              bottomShadow.style.display =
-                entry.isIntersecting || hide(Placement.bottom, direction)
-                  ? DisplayValue.none
-                  : DisplayValue.block
-              break
-            case leftTrigger:
-              leftShadow.style.display =
-                entry.isIntersecting || hide(Placement.left, direction)
-                  ? DisplayValue.none
-                  : DisplayValue.block
-              break
-          }
-        }
-      },
-      {
-        root: scrollable,
-      },
-    )
-    intersectionObserver.observe(topTrigger)
-    intersectionObserver.observe(rightTrigger)
-    intersectionObserver.observe(bottomTrigger)
-    intersectionObserver.observe(leftTrigger)
-
-    return () => {
-      ;[
-        topTrigger,
-        rightTrigger,
-        bottomTrigger,
-        leftTrigger,
-        topShadow,
-        rightShadow,
-        bottomShadow,
-        leftShadow,
-      ].forEach(target => target.remove())
-      intersectionObserver.disconnect()
-      resizeObserver.disconnect()
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  useScrollShadows(ref, shadowsRef, direction)
 
   return (
     <div className={cn(styles.container, className)}>
